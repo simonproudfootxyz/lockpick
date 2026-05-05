12b4835f0f62d9c62335114ad6dbf5ecdf5dc38e
simonproudfootxyz <simon.d.proudfoot@gmail.com>
2026-05-04 23:12:26 -0400

    - add rules button to game header

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -1,6 +1,8 @@
 import React from "react";
 import LockpickLogo from "../assets/LockpickLogo.svg";
 import "./GameHeader.css";
+import { TextButton } from "./Button";
+import HelpIcon from "../assets/HelpIcon.svg";
 
 const GameHeader = ({ children }) => {
   return (
@@ -8,6 +10,13 @@ const GameHeader = ({ children }) => {
       <h1 className="game-header__title">
         <img src={LockpickLogo} alt="Lockpick" />
       </h1>
+      <TextButton
+        className="game-header__rules-trigger"
+        onClick={() => console.log("onclick running")}
+      >
+        <img src={HelpIcon} alt="How do I play?" width={16} height={16} />
+        <span className="hidden--tablet-down">How do I play?</span>
+      </TextButton>
       {children}
     </div>
   );
